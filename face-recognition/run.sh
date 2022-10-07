783c679e00e857b8b1c05c3b507743d7d9664d0a
tjdhg456 <hogili89@gist.ac.kr>
2022-10-07 18:08:16 +0900

    --

--- face-recognition/run.sh
+++ face-recognition/run.sh
@@ -3,7 +3,7 @@ python train_stage1.py --gpus 2 --down_size 0 --seed 0 --mode ir --backbone ires
 
 
 # Student Training
-for resol in 14 28 56 1
+for resol in 1 14 28 56
 do
-    python train_stage2.py --gpus 2 --down_size $resol --seed 0 --mode ir --backbone iresnet50 --margin_type CosFace --save_dir /data/sung/checkpoint/CVPR_FACE/qualnet_student/down_$resol/iresnet50-ir --pretrained_student True --teacher_path /data/sung/checkpoint/CVPR_FACE/qualnet_teacher/high/iresnet50-ir/last_net.ckpt
+    python train_stage2.py --gpus 0 --down_size $resol --seed 0 --mode ir --backbone iresnet50 --margin_type CosFace --save_dir /data/sung/checkpoint/CVPR_FACE/qualnet_student/down_$resol/iresnet50-ir --pretrained_student True --teacher_path /data/sung/checkpoint/CVPR_FACE/qualnet_teacher/high/iresnet50-ir/last_net.ckpt
 done
